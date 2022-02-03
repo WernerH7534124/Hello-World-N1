f99f5e047eb5db192071a0ba5916e1c3b0514c88
Werner Harak <wharak@tktk.ee>
2022-02-03 15:28:59 +0200

    huvitav kas toimis

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello! - What is your name?");
+			
+			string OPName;
+			
+			OPName = Console.ReadLine();
+
+			Console.WriteLine("Nice to meet you " + OPName + "!");
+
+		Console.Read();
+
+		}
+	}
+}
